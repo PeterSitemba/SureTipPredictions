e34cde78fb56191f9f37faaec9bea5f45ed7233c
Sitemba <omarorops@gmail.com>
2022-01-02 23:41:03 +0300

    Added animations to expanding list

--- app/src/main/java/faba/app/suretippredictions/Constants.kt
+++ app/src/main/java/faba/app/suretippredictions/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val URL: String =
         "https://fqq4pp5xhjbijggrrxnsl62szq.appsync-api.me-south-1.amazonaws.com/graphql/"
 
-    val mainLeaguesList: List<Int> = arrayListOf(39, 71, 173, 421, 847, 432, 59, 441, 149, 430, 383)
+    val mainLeaguesList: List<Int> = arrayListOf(39, 61, 78, 88, 135, 140, 173, 421, 847, 432, 441, 149, 430, 383)
 
     const val EXPAND_ANIMATION_DURATION = 300
     const val COLLAPSE_ANIMATION_DURATION = 300
